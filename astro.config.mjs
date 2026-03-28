5af4eff4a4a916560dff0793af37e19ae1875f30
Moisés Rodríguez <65286072+moisesrj97@users.noreply.github.com>
2026-03-28 15:55:00 +0000

    chore: add remark breaks plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import remarkBreaks from "remark-breaks";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,6 +8,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
+
+	markdown: {
+		remarkPlugins: [remarkBreaks],
+	},
 
 	vite: {
 		plugins: [tailwindcss()],
